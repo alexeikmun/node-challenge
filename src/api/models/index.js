5fd2b71dfaa47c201bf4b07a26342965d7f7c854
Leonel Contreras <leonel.contrerasn@gmail.com>
2021-10-29 23:09:58 -0500

    First draft: GET: /notification-frequency

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -1,5 +1,5 @@
 export { default as UserModel } from './user.model'
 export { default as GoalModel } from './goal.model'
-export {  GoalTypeModel } from './goal-type.model'
-export {  GoalStatusModel } from './goal-status.model'
+export { GoalTypeModel } from './goal-type.model'
+export { GoalStatusModel } from './goal-status.model'
 export { NotificationFrequencyModel } from './notification-frequency.model'
