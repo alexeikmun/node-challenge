51be47b68b179640cd6ef62f747cf70056d54588
Leonel Contreras <leonel.contrerasn@gmail.com>
2021-11-03 22:10:47 -0600

    Last draft of right-things today

--- src/api/validators/generic.validator.js
+++ src/api/validators/generic.validator.js
@@ -1,8 +1,9 @@
-import { param } from 'express-validator'
+import { param, query } from 'express-validator'
 import i18n from 'i18n'
 
 const idValidator = param('id').isMongoId().notEmpty().withMessage(() => i18n.__('generic-validator.id'))
-
+const timeZoneValidator = query('timeZone').isString().notEmpty(() => i18n.__('generic-validator.time-zone'))
 export {
-  idValidator
+  idValidator,
+  timeZoneValidator
 }
