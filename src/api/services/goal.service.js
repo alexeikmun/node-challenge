9a6f5de29f0b66fa0b2024ffed8f93c7b37d1a34
Leonel Contreras <leonel.contrerasn@gmail.com>
2021-11-08 22:06:45 -0600

    Final draft delete goal

--- src/api/services/goal.service.js
+++ src/api/services/goal.service.js
@@ -3,7 +3,7 @@ import { GoalModel } from '../models'
 const GoalService = () => {
   const createGoal = async (goal) => await GoalModel.create(goal)
   const updateGoal = async (id, query) => await GoalModel.updateOne({ _id: id }, query)
-  const deleteGoal = async (id) => await GoalModel.deleteOne({ _id: id })
+  const deleteGoal = async (query) => await GoalModel.deleteOne(query)
   const findGoals = async (query) => await GoalModel.find(query)
   const findGoal = async (query) => await GoalModel.findOne(query)
   const countGoal = async (query) => await GoalModel.countDocuments(query)
--- src/api/validators/generic.validator.js
+++ src/api/validators/generic.validator.js
@@ -3,6 +3,7 @@ import i18n from 'i18n'
 
 const idValidator = param('id').isMongoId().notEmpty().withMessage(() => i18n.__('generic-validator.id'))
 const timeZoneValidator = query('timeZone').isString().notEmpty(() => i18n.__('generic-validator.time-zone'))
+
 export {
   idValidator,
   timeZoneValidator
